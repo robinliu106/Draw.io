bb5868937e0293958d4d44a1fba8acc860f20b17
Robin Liu <robinliu106@gmail.com>
2020-04-19 16:33:29 -0400

    allow user to join specific room

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,6 +14,11 @@ const locationMessageTemplate = document.querySelector(
     "#location-message-template"
 ).innerHTML;
 
+//Options
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
+
 socket.on("message", (message) => {
     const html = Mustache.render(messageTemplate, {
         message: message.text,
@@ -71,3 +76,5 @@ $sendLocationButton.addEventListener("click", (e) => {
         });
     });
 });
+
+socket.emit("join", { username, room });
