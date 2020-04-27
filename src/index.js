68a9a81934a3eabff30cf4f85406edcb2aa717eb
Robin Liu <robinliu106@gmail.com>
2020-04-27 16:38:02 -0400

    move async getTranslate to own function

--- src/index.js
+++ src/index.js
@@ -61,68 +61,20 @@ io.on("connection", (socket) => {
     socket.on("sendMessage", async (message, callback) => {
         const filter = new Filter();
         const user = getUser(socket.id);
+        const doTranslate = true;
 
         if (filter.isProfane(message)) {
             return callback("Profanity is not allowed");
         }
 
-        //async await version
-        const translationPromise = util.promisify(googleTranslate.translate);
-
-        try {
-            const translateOne = await translationPromise(message, "es"); //translate to another language
-
-            //translate back to english
-            const translationTwo = await translationPromise(
-                translateOne.translatedText,
-                "en"
-            );
-
-            //emit english message
-            io.to(user.room).emit(
-                "message",
-                generateMessage(user.username, translationTwo.translatedText)
-            );
-        } catch (error) {
-            console.log("Translation Error: ", error);
+        if (doTranslate) {
+            message = await getTranslation(message);
         }
 
-        //callback version
-        // googleTranslate.translate(message, "zh", (error, translation) => {
-        //     const translationOne = translation.translatedText;
-        //     googleTranslate.translate(
-        //         translationOne,
-        //         "en",
-        //         (error, translationTwo) => {
-        //             const messageFinal = translationTwo.translatedText;
-
-        //             io.to(user.room).emit(
-        //                 "message",
-        //                 generateMessage(user.username, messageFinal)
-        //             );
-        //         }
-        //     );
-        // });
-
-        //promise version
-        // const translationPromise = util.promisify(googleTranslate.translate);
-
-        // translationPromise(message, "zh")
-        //     .then((translatedOne) => {
-        //         console.log("translatedOne", translatedOne);
-        //         translationPromise(translatedOne.translatedText, "en")
-        //             .then((translateTwo) => {
-        //                 io.to(user.room).emit(
-        //                     "message",
-        //                     generateMessage(
-        //                         user.username,
-        //                         translateTwo.translatedText
-        //                     )
-        //                 );
-        //             })
-        //             .catch((error) => console.log(error));
-        //     })
-        //     .catch((error) => console.log(error));
+        io.to(user.room).emit(
+            "message",
+            generateMessage(user.username, message)
+        );
 
         callback();
     });
@@ -161,6 +113,64 @@ io.on("connection", (socket) => {
     });
 });
 
+const getTranslation = async (message) => {
+    //async await version
+    const translationPromise = util.promisify(googleTranslate.translate);
+
+    try {
+        const translateOne = await translationPromise(message, "es"); //translate to another language
+
+        //translate back to english
+        const translationTwo = await translationPromise(
+            translateOne.translatedText,
+            "en"
+        );
+
+        return translationTwo.translatedText;
+    } catch (error) {
+        console.log("Translation Error: ", error);
+
+        return "Message could not be translated";
+    }
+};
+
 server.listen(port, () => {
     console.log(`Server is up on port: ${port}`);
 });
+
+//callback version of translate message
+// googleTranslate.translate(message, "zh", (error, translation) => {
+//     const translationOne = translation.translatedText;
+//     googleTranslate.translate(
+//         translationOne,
+//         "en",
+//         (error, translationTwo) => {
+//             const messageFinal = translationTwo.translatedText;
+
+//             io.to(user.room).emit(
+//                 "message",
+//                 generateMessage(user.username, messageFinal)
+//             );
+//         }
+//     );
+// });
+
+//promise version of translate message
+// const translationPromise = util.promisify(googleTranslate.translate);
+
+// translationPromise(message, "zh")
+//     .then((translatedOne) => {
+//         console.log("translatedOne", translatedOne);
+//         translationPromise(translatedOne.translatedText, "en")
+//             .then((translateTwo) => {
+//                 io.to(user.room).emit(
+//                     "message",
+//                     generateMessage(
+//                         user.username,
+//                         translateTwo.translatedText
+//                     )
+//                 );
+//             })
+//             .catch((error) => console.log(error));
+//     })
+//     .catch((error) => console.log(error));
