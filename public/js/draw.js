ebd6913bfb86c0b63c22657d21598728eadc4c00
Robin Liu <robinliu106@gmail.com>
2020-05-10 18:20:45 -0400

    added buttons to change color and fixed color issue

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,15 +1,30 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+canvas.width = (window.innerWidth * 2) / 3;
+canvas.height = window.innerHeight * 0.9;
+
+context.strokeStyle = "black";
+// const $changeColorBlue = document.querySelector("#change-color-blue");
+
+// $changeColorBlue.addEventListener("click", (e) => {
+//     drawColor = "blue";
+// });
+
+function changeColor(color) {
+    console.log("changing color to ", color);
+    context.strokeStyle = color;
+}
+
 function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
+    context.closePath();
+    // context.strokeStyle = drawColor;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    canvas.width = (window.innerWidth / 3) * 2;
-    canvas.height = window.innerHeight;
-
     var drawing = false;
     var x, y, prevX, prevY;
 
@@ -28,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
     canvas.onmousemove = function (e) {
         x = e.clientX - 220;
         y = e.clientY;
-        console.log("x,y", x, y);
+
         if (drawing) {
             socket.emit("draw", {
                 x1: prevX,
